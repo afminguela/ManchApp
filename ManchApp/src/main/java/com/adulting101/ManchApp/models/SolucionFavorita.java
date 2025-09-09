16b194a3d9a9b9bef98bf1949607691d92031235
Ana Fernandez Minguela <112387723+afminguela@users.noreply.github.com>
2025-09-09 20:23:42 +0200

    9sep2023

---
+++ ManchApp/src/main/java/com/adulting101/ManchApp/models/SolucionFavorita.java
@@ -0,0 +1,18 @@
+package com.adulting101.ManchApp.models;
+
+import com.adulting101.ManchApp.enums.Nivel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table (name ="soluciones_favoritas")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class SolucionFavorita {
+
+}
--- ManchApp/src/main/java/com/adulting101/ManchApp/models/SolucionLimpieza.java
+++ ManchApp/src/main/java/com/adulting101/ManchApp/models/SolucionLimpieza.java
@@ -13,34 +13,60 @@ import lombok.NoArgsConstructor;
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
+    @Builder
     public class SolucionLimpieza {
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
-
+       
+        @NotNull(message = "El titulo es obligatorio")
+        @Column(name="titulo")
         private String titulo;
+
+        @NotNull(message = "Las instrucciones se han de consignar")
+        @Column(name="instrucciones")
         private String instrucciones;
+
+        @Min (value = 1, message =" el tiempo minimo es un minuto")
         private Integer tiempoMinutos;
 
 
         @Enumerated(EnumType.STRING)
+        @Column(name ="dificultad")
         private Nivel dificultad;
 
-        private Integer efectividad;
+        @Column(name ="efectividad")
+        private Nivel efectividad;
 
         @ManyToOne(fetch = FetchType.LAZY)
         private Precauciones advertencias;
 
         private String consejos;
-        private boolean casero;
-
 
         private String categoria;
 
         @ManyToOne(fetch = FetchType.LAZY)
-        private Material material;
+        private List<Material> material;
 
         @ManyToOne(fetch = FetchType.LAZY)
-        private TipoMancha mancha;
+        private List<TipoMancha> mancha;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        private List<Utensilio> utensilios
+
+        -Long id
+        -String titulo
+        -String instrucciones
+        -Integer tiempoMinutos
+        -Nivel dificultad
+        -Integer efectividad
+        -Precauciones advertencias
+        -String consejos
+        -boolean casero
+        -List<Utensilio> utensilios
+        -List<Ingrediente> ingredientes
+        -List<TipoMancha> manchas
+        -Categoria categoria
+        -LocalDateTime fechaCreacion
 }
