4fe19cc4984507ec7cafae13b90a194a8a3df35d
afminguela <afminguela@gmail.com>
2025-09-12 20:18:42 +0200

    Añadido Exception Handlers

--- ManchApp/src/test/java/com/adulting101/ManchApp/controllers/SolucionLimpiezaControllerTest.java
+++ ManchApp/src/test/java/com/adulting101/ManchApp/controllers/SolucionLimpiezaControllerTest.java
@@ -2,14 +2,40 @@ package com.adulting101.ManchApp.controllers;
 
 import com.adulting101.ManchApp.DTO.tipoMancha.TipoManchaRequestDTO;
 import com.adulting101.ManchApp.models.SolucionLimpieza;
+import com.adulting101.ManchApp.services.impl.SolucionLimpiezaImpl;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.util.*;
 
+@WebMvcTest(SolucionLimpiezaController.class)
 class SolucionLimpiezaControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SolucionLimpiezaImpl service;
+
+    @Test
+    public void getSolucionLimpiezaById_retorna200() throws Exception {
+
+
+        Mockito.when(service.getSolucionLimpiezaById(1L)).thenReturn(inmueble);
+
+        mockMvc.perform(get("/api/inmuebles/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.titulo").value("Piso en Málaga"));
+    }
+
 
     @Test
     void getSolucionLimpiezaReturnsAllSolutionsWhenNoFiltersProvided() {
@@ -40,11 +66,7 @@ class SolucionLimpiezaControllerTest {
         verify(service).listaSolucionLimpieza(tipoMancha, null);
     }
 
-    @Test
-    void getSolucionLimpieza() {
 
-
-    }
 
     @Test
     void getSolucionLimpiezaPorId() {
